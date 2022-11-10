1ab4dfa5b247e986898a5d15355f1ed08ac0da9b
Quazi_Shams_Kabir <quaziraj12@gmail.com>
2022-11-10 20:23:05 +0530

    isPresent added

---
+++ isPresent.java
@@ -0,0 +1,15 @@
+public class isPresent {
+    public static void main(String[] args) {
+        int[] arr = {18,1,84,15,484,5,48,15};  //if there is an unsorted array you have to do linear search
+        int target = 84;
+        System.out.println(isPresent(arr,target));
+    }
+    static boolean isPresent(int [] arr,int target){
+        for (int i : arr) {
+            if (i == target) {
+                return true;
+            }
+        }
+            return false;
+    }
+}
